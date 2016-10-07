7e67c787d3d413d6a1bd445cc1237890955f9a22
domaincrisman <cmanzanore@gmail.com>
2016-10-07 17:04:51 +0200

    Devise instalation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007112718) do
+ActiveRecord::Schema.define(version: 20161007145129) do
 
   create_table "org_companies", force: :cascade do |t|
     t.string   "name",             null: false
@@ -59,17 +59,35 @@ ActiveRecord::Schema.define(version: 20161007112718) do
   create_table "org_people", force: :cascade do |t|
     t.integer  "typ_position_id"
     t.integer  "org_company_id"
-    t.string   "first_name",             null: false
-    t.string   "last_name",              null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
     t.string   "stripe_publishable_key"
     t.string   "stripe_secret_key"
     t.string   "stripe_user_id"
     t.string   "stripe_currency"
     t.string   "stripe_account_type"
     t.text     "stripe_account_status"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
+
+  add_index "org_people", ["confirmation_token"], name: "index_org_people_on_confirmation_token", unique: true
+  add_index "org_people", ["email"], name: "index_org_people_on_email", unique: true
+  add_index "org_people", ["reset_password_token"], name: "index_org_people_on_reset_password_token", unique: true
 
   create_table "org_products", force: :cascade do |t|
     t.integer  "org_company_id"
